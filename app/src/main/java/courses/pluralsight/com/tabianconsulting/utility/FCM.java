e988ec7543e3d0bf35141c3a3de9c22b832074c6
Fresh-Teacher <98003374+Fresh-Teacher@users.noreply.github.com>
2022-12-16 09:39:30 -0800

    Initial commit

---
+++ app/src/main/java/courses/pluralsight/com/tabianconsulting/utility/FCM.java
@@ -0,0 +1,24 @@
+package courses.pluralsight.com.tabianconsulting.utility;
+
+import java.util.Map;
+
+import courses.pluralsight.com.tabianconsulting.models.fcm.FirebaseCloudMessage;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.POST;
+
+/**
+ * Created by User on 11/16/2017.
+ */
+
+public interface FCM {
+
+    @POST("send")
+    Call<ResponseBody> send(
+            @HeaderMap Map<String, String> headers,
+            @Body FirebaseCloudMessage message
+            );
+}
